f5ed6ca96813ccbaae36c84b4a8d048371212296
ElieF <elie.froucht@viseo.com>
2020-03-24 16:21:44 +0100

    url

--- src/js/useFetch.js
+++ src/js/useFetch.js
@@ -5,12 +5,12 @@ export const useFetch = url => {
   const [data, updateData] = useState(undefined);
   useEffect(() => {
     async function fetchData() {
-      console.log("je load les data")
+      console.log("je load les data url: " + url)
       const resp = await axios.get("https://improparis.com/gestion/postulant/AjaxLevelAppreciation.php");
 
       updateData(resp.data);
     }
     fetchData();
-  }, []);
+  }, [url]);
   return data;
 };
