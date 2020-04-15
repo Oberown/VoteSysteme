ebf5a4da17dae735227970b41bd39af43c8a8c82
ElieF <elie.froucht@viseo.com>
2020-04-15 17:17:54 +0200

    meilleur presentation

--- src/js/Postulant.js
+++ src/js/Postulant.js
@@ -4,10 +4,10 @@ class Postulant extends React.Component {
   render() {
     const postulantData = this.props.profile;
     return (<div>
-      {postulantData.name}
+      <div>{postulantData.name}</div>
       {postulantData.photo !== ''  ?
       (<img src={postulantData.photo} alt="" />)
-      : ''
+      : <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRAoHz7UBgIBZMCV3E0hdgSSfMjSMfkhMuC446PpDqUR3HkPV6qw&s' alt="" />
       }    
     </div>);
   }
